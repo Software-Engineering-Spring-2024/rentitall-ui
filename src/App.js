17fbc19e1305873a71068c61e4e148692f28f8d7
Pranay Namburi <pranayn1999@gmail.com>
2024-02-27 03:14:02 -0500

    Update NavBar design and show it in all pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import './variables.css';
 import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 import {LoginPage} from "./screens/login-page/LoginPage";
 import {SignupPage} from "./screens/signup-page/SignupPage";
@@ -8,6 +9,7 @@ import {ForgotPasswordPage} from "./screens/forgot-password/ForgotPasswordPage";
 import {ResetPassword} from "./screens/reset-password/ResetPassword";
 import {ProtectedRoute} from "./wrapper/ProtectedRoute";
 import {createContext} from "react";
+import { NavBar } from "./components/NavBar";
 
 const UserContext = createContext();
 function App() {
@@ -15,6 +17,7 @@ function App() {
       <Router>
           <div className="App h-screen">
               <div className='routes'>
+                  <NavBar />
                   <Routes>
                       <Route exact path='/' element={<Navigate to='/home'/>}/>
                       <Route exact path='/login' element={<LoginPage/>}/>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import { useCallback } from 'react';
 import {Navigate, useNavigate} from "react-router-dom";
+import './../styles/NavBar.css'
 
 export const NavBar = () => {
     const navigate = useNavigate();
@@ -8,11 +9,10 @@ export const NavBar = () => {
     })
 
     return (
-        <div className="Navbar px-4 py-2 flex justify-between bg-white">
+        <div className="NavBar">
             <div></div>
             <button
                 onClick={goToLoginPage}
-                className="flex justify-center rounded-md bg-dark-green px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-opacity-80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
                 Sign in
             </button>
