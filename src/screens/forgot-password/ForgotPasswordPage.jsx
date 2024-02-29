3a34fc352ef82ccc531744cb4f81fbbdf9c828d6
Pranay Namburi <pranayn1999@gmail.com>
2024-02-28 21:04:31 -0500

    Forgt password changes

--- src/screens/forgot-password/ForgotPasswordPage.jsx
+++ src/screens/forgot-password/ForgotPasswordPage.jsx
@@ -1,18 +1,20 @@
-import {Link, useNavigate} from "react-router-dom";
-import {useCallback, useEffect, useState} from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useCallback, useEffect, useState } from "react";
 import validator from "validator";
 import axios from "axios";
-import {BackArrowButton} from "../../components/BackArrowButton";
+import { BackArrowButton } from "../../components/BackArrowButton";
+import './../../styles/ForgotPassword.css'
+
 export const ForgotPasswordPage = () => {
     const navigate = useNavigate();
-    const [email,setEmail] = useState('');
-    const [verificationCode,setVerificationCode] = useState('');
-    const [regenerate,setRegenerate] = useState(false);
-    const [displayMessage,setDisplayMessage] = useState('');
-    const [startTimer,setStartTimer] = useState(false);
+    const [email, setEmail] = useState('');
+    const [verificationCode, setVerificationCode] = useState('');
+    const [regenerate, setRegenerate] = useState(false);
+    const [displayMessage, setDisplayMessage] = useState('');
+    const [startTimer, setStartTimer] = useState(false);
     const [timer, setTimer] = useState(60);
-    const [displayMessageColor,setDisplayMessageColor] = useState('');
-    const [generatedOtp,setGeneratedOtp] = useState('');
+    const [displayMessageColor, setDisplayMessageColor] = useState('');
+    const [generatedOtp, setGeneratedOtp] = useState('');
     useEffect(() => {
         let interval = null;
         if (startTimer) {
@@ -25,7 +27,7 @@ export const ForgotPasswordPage = () => {
         return () => clearInterval(interval);
     }, [startTimer, timer]);
 
-    function sentSuccess(response){
+    function sentSuccess(response) {
         setDisplayMessageColor('text-dark-green');
         setGeneratedOtp(response.data.otp);
         setDisplayMessage(response.data.message);
@@ -41,18 +43,19 @@ export const ForgotPasswordPage = () => {
         setStartTimer(true);
         setTimeout(() => {
             setStartTimer(false);
-        },60000)
+        }, 60000)
     }
-    async function callApi(req){
-        try{
-            const response = await axios.post(process.env.REACT_APP_LOGIN_SERVICE + '/sendOtp',{'email': email},{headers:
-                    {
-                        "Content-Type": "application/json"
-                    }
+    async function callApi(req) {
+        try {
+            const response = await axios.post(process.env.REACT_APP_LOGIN_SERVICE + '/sendOtp', { 'email': email }, {
+                headers:
+                {
+                    "Content-Type": "application/json"
+                }
             });
             sentSuccess(response);
         }
-        catch (e){
+        catch (e) {
             console.error('Request failed:', e);
             if (e.response && e.response.status === 500) {
                 handleErrorResponse(e.response);
@@ -66,9 +69,9 @@ export const ForgotPasswordPage = () => {
             setTimer(60);
             e.preventDefault();
             if (email !== '' && validator.isEmail(email)) {
-                await callApi({email: email})
+                // await callApi({ email: email })
             }
-            else{
+            else {
                 setDisplayMessageColor('text-red-600');
                 setDisplayMessage("Enter a Valid Email Address..");
             }
@@ -79,85 +82,85 @@ export const ForgotPasswordPage = () => {
         e.preventDefault();
         console.log(generatedOtp);
         console.log(verificationCode);
-        verificationCode === generatedOtp ? navigate('/reset-password',{state:{email:email,allowed:true}}): setDisplayMessageColor('text-red-600');
+        verificationCode === generatedOtp ? navigate('/reset-password', { state: { email: email, allowed: true } }) : setDisplayMessageColor('text-red-600');
         setDisplayMessage('Enter a Valid Verification Code');
     }
 
     return (
-        <div className="flex min-h-full flex-1 flex-col justify-self-center px-6 py-28 lg:px-8">
-            <div className="sm:mx-auto sm:w-full sm:max-w-sm basis-1">
-                <div className='text-left'><BackArrowButton path='/login'/></div>
-                <img
-                    className="mx-auto h-28 w-auto content-brandLogo"
-                    alt="Your Company"
-                />
-                <h2 className="mt-2 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                    Forgot Your Password?
-                </h2>
-            </div>
-
-            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6" onSubmit={verifyCode}>
-                    <div>
-                        <div className="flex items-center justify-between">
-                            <label htmlFor="email"
-                                   className="block text-sm font-medium text-left leading-6 text-gray-900">
-                                Email address
-                            </label>
-                            <div className="text-sm">
-                                <a href='#' onClick={sendCodeToEmail}
-                                   className={"text-blue-700 hover:text-blue-500 underline font-bold "+ (startTimer? 'pointer-events-none opacity-50' : '')}>
-                                    Generate OTP
-                                </a>
-                                <>{startTimer && <span className="text-sm font-normal">  :{timer}S</span>}</>
+        <div className="ForgotPasswordPage">
+            <div className="forgot-password-container">
+                <div className="forgot-password-section">
+                    <div className="forgot-password-top-section">
+                        <BackArrowButton size='20' className="back-button" path='/login' />
+                        <img
+                            className="company-logo content-brandLogo"
+                            alt="RentItAll"
+                        />
+                        <div className="forgot-password-title-msg-wrapper">
+                            <h1 className="forgot-password-title-msg">
+                                Create a New Account
+                            </h1>
+                        </div>
+                    </div>
+                    <div className="forgot-password-body-section">
+                        <form className="" onSubmit={verifyCode}>
+                            <div className="field-wrapper">
+                                <label htmlFor="email" className="">
+                                    Email address
+                                </label>
+                                <div className="input-wrapper">
+                                    <div className="generate-otp-action">
+                                        <a href='#' onClick={sendCodeToEmail}
+                                            className={(startTimer ? 'pointer-events-none opacity-50' : '')}>
+                                            Generate OTP
+                                        </a>
+                                        <>{startTimer && <span className="text-sm font-normal">  :{timer}S</span>}</>
+                                    </div>
+                                    <input
+                                        id="email"
+                                        name="email"
+                                        type="email"
+                                        value={email}
+                                        autoComplete="email"
+                                        placeholder="Enter Your Email"
+                                        required
+                                        onChange={(e) => {
+                                            setEmail(e.target.value);
+                                        }}
+                                    />
+                                </div>
+                                <>{displayMessage !== '' && <div className="error-message" style={{ marginTop: 0 }}><span className={(displayMessageColor)}>{displayMessage}</span></div>}</>
                             </div>
-                        </div>
-                        <div className="">
-                            <input
-                                id="email"
-                                name="email"
-                                type="email"
-                                value={email}
-                                autoComplete="email"
-                                placeholder="Enter Your Email"
-                                required
-                                onChange={(e) => {
-                                    setEmail(e.target.value);
-                                }}
-                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            />
-                        </div>
-                    </div>
-                    <>{displayMessage!=='' && <div className={"text-left"} style={{marginTop:0}}><label className={"text-xs font-normal "+(displayMessageColor)}>{displayMessage}</label></div> }</>
-                    <div className="text-left">
-                        <label htmlFor="verificationCode" className="block text-sm text-left font-medium leading-6 text-gray-900">
-                            Verification Code
-                        </label>
-                        <div className="">
-                            <input
-                                id="verificationCode"
-                                name="verificationCode"
-                                type="text"
-                                value={verificationCode}
-                                onChange={(e) => {
-                                    setVerificationCode(e.target.value);
-                                }}
-                                placeholder="Enter Confirmation Code"
-                                required
-                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            />
-                        </div>
-                    </div>
+                            <div className="field-wrapper">
+                                <label htmlFor="verificationCode" className="">
+                                    Verification Code
+                                </label>
+                                <div className="input-wrapper">
+                                    <input
+                                        id="verificationCode"
+                                        name="verificationCode"
+                                        type="text"
+                                        value={verificationCode}
+                                        onChange={(e) => {
+                                            setVerificationCode(e.target.value);
+                                        }}
+                                        placeholder="Enter Confirmation Code"
+                                        required
+                                    />
+                                </div>
+                            </div>
 
-                    <div>
-                        <button
-                            type="submit"
-                            className="flex w-full justify-center rounded-md bg-dark-green px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-opacity-80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                        >
-                            Verify Code
-                        </button>
+                            <div>
+                                <button
+                                    type="submit"
+                                    className="verify-code-button"
+                                >
+                                    Verify Code
+                                </button>
+                            </div>
+                        </form>
                     </div>
-                </form>
+                </div>
             </div>
         </div>
     )
