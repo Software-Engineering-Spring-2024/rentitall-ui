39fb07ecf572e35f29560b8bd5fb6124418ebe0f
Pranay Namburi <pranayn1999@gmail.com>
2024-03-03 17:16:49 -0500

    Restrict unsigned user to list a product

--- src/components/ProductListModal.jsx
+++ src/components/ProductListModal.jsx
@@ -1,7 +1,9 @@
-import React, { useCallback, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { Modal } from "./Modal"
 import './../styles/ProductListModal.css'
 import { GrClose } from "react-icons/gr";
+import axios from "axios";
+import { useUser } from '../hooks/UserContext';
 
 export const ProductListModal = (props) => {
     const categories = [
@@ -35,6 +37,7 @@ export const ProductListModal = (props) => {
         imageBase64URL: ""
     })
     const imageInputRef = useRef(null);
+    const { user, loginData } = useUser();
 
     const handleChange = (e) => {
         const { name, value } = e.target; // Destructure the name and value from the event target
@@ -54,8 +57,8 @@ export const ProductListModal = (props) => {
             reader.onloadend = () => {
                 const base64String = reader.result;
                 setProductDetails({
-                    ...productDetails, // Spread the current formData
-                    imageBase64URL: base64String // Update the value of the specific field
+                    ...productDetails,
+                    imageBase64URL: base64String
                 });
             };
 
@@ -78,114 +81,144 @@ export const ProductListModal = (props) => {
         }
     }
 
-    const handleSubmitProductListDetails = useCallback((e) => {
+    const handleSubmitProductListDetails = useCallback(async (e) => {
         e.preventDefault()
         console.log(productDetails)
     })
 
+    const handleModalClose = useCallback(() => {
+        setProductDetails({
+            title: "",
+            description: "",
+            price_per_day: "",
+            categoryId: "",
+            ownerId: user.user_id,
+            imageBase64URL: ""
+        })
+        props.closeModal()
+    })
+
+    useEffect(() => {
+        console.log(user)
+        if (user?.user_id) {
+            setProductDetails({
+                ...productDetails,
+                ownerId: user.user_id
+            });
+        }
+    }, [user])
+
     return (
         <React.Fragment>
-            <Modal show={props.show} closeModal={props.closeModal} title="Enter your product details to list in Rent It All">
-                <div className="product-listing-details-form">
-                    <form className="" onSubmit={handleSubmitProductListDetails}>
-                        <div className="field-wrapper">
-                            <label htmlFor="title" className="">
-                                Product Title
-                            </label>
-                            <div className="input-wrapper">
-                                <input
-                                    id="title"
-                                    name="title"
-                                    type="text"
-                                    value={productDetails.title}
-                                    placeholder="Enter your Product's Name"
-                                    required
-                                    onChange={handleChange}
-                                />
-                            </div>
+            {
+                loginData.isLoggedIn ? (
+                    <Modal show={props.show} closeModal={handleModalClose} title="Enter your product details to list in Rent It All">
+                        <div className="product-listing-details-form">
+                            <form className="" onSubmit={handleSubmitProductListDetails}>
+                                <div className="field-wrapper">
+                                    <label htmlFor="title" className="">
+                                        Product Title
+                                    </label>
+                                    <div className="input-wrapper">
+                                        <input
+                                            id="title"
+                                            name="title"
+                                            type="text"
+                                            value={productDetails.title}
+                                            placeholder="Enter your Product's Name"
+                                            required
+                                            onChange={handleChange}
+                                        />
+                                    </div>
+                                </div>
+                                <div className="field-wrapper">
+                                    <label htmlFor="description" className="">
+                                        Product Description
+                                    </label>
+                                    <div className="input-wrapper">
+                                        <textarea
+                                            id="description"
+                                            name="description"
+                                            type="text"
+                                            value={productDetails.description}
+                                            placeholder="Enter your Product's description"
+                                            required
+                                            onChange={handleChange}
+                                        />
+                                    </div>
+                                </div>
+                                <div className="field-wrapper">
+                                    <label htmlFor="price" className="">
+                                        Price
+                                    </label>
+                                    <div className="input-wrapper">
+                                        <input
+                                            id="price"
+                                            name="price_per_day"
+                                            type="number"
+                                            value={productDetails.price_per_day}
+                                            placeholder="Enter your product price per day"
+                                            required
+                                            onChange={handleChange}
+                                        />
+                                    </div>
+                                </div>
+                                <div className="field-wrapper">
+                                    <label htmlFor="categoryId" className="">
+                                        Category
+                                    </label>
+                                    <div className="input-wrapper">
+                                        <select name="categoryId" value={productDetails.categoryId} onChange={handleChange}>
+                                            <option value="" disabled>Please select the category</option>
+                                            {
+                                                categories.map(category => <option key={category.id} value={category.id}>{category.name}</option>)
+                                            }
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="field-wrapper">
+                                    <label htmlFor="product_image" className="">
+                                        Upload image
+                                    </label>
+                                    <div className="input-wrapper">
+                                        <input
+                                            id="product_image"
+                                            name="image"
+                                            type="file"
+                                            accept=".jpg, .jpeg, .png"
+                                            required
+                                            ref={imageInputRef}
+                                            onChange={handleFileUpload}
+                                        />
+                                        {
+                                            productDetails.imageBase64URL.length > 0 && (
+                                                <div className="imagesPreview">
+                                                    <div className="imagePreview">
+                                                        <img src={productDetails.imageBase64URL} alt="" />
+                                                        <span className="remove-image" onClick={removeUploadedImage}><GrClose size="8" /></span>
+                                                    </div>
+                                                </div>
+                                            )
+                                        }
+                                    </div>
+                                </div>
+                                <div>
+                                    <button
+                                        type="submit"
+                                        className="list-item-button"
+                                    >
+                                        List item
+                                    </button>
+                                </div>
+                            </form>
                         </div>
-                        <div className="field-wrapper">
-                            <label htmlFor="description" className="">
-                                Product Description
-                            </label>
-                            <div className="input-wrapper">
-                                <textarea
-                                    id="description"
-                                    name="description"
-                                    type="text"
-                                    value={productDetails.description}
-                                    placeholder="Enter your Product's description"
-                                    required
-                                    onChange={handleChange}
-                                />
-                            </div>
-                        </div>
-                        <div className="field-wrapper">
-                            <label htmlFor="price" className="">
-                                Price
-                            </label>
-                            <div className="input-wrapper">
-                                <input
-                                    id="price"
-                                    name="price_per_day"
-                                    type="number"
-                                    value={productDetails.price_per_day}
-                                    placeholder="Enter your product price per day"
-                                    required
-                                    onChange={handleChange}
-                                />
-                            </div>
-                        </div>
-                        <div className="field-wrapper">
-                            <label htmlFor="categoryId" className="">
-                                Category
-                            </label>
-                            <div className="input-wrapper">
-                                <select name="categoryId" value={productDetails.categoryId} onChange={handleChange}>
-                                    <option value="" disabled>Please select the category</option>
-                                    {
-                                        categories.map(category => <option key={category.id} value={category.id}>{category.name}</option>)
-                                    }
-                                </select>
-                            </div>
-                        </div>
-                        <div className="field-wrapper">
-                            <label htmlFor="product_image" className="">
-                                Upload image
-                            </label>
-                            <div className="input-wrapper">
-                                <input
-                                    id="product_image"
-                                    name="image"
-                                    type="file"
-                                    accept=".jpg, .jpeg, .png"
-                                    required
-                                    ref={imageInputRef}
-                                    onChange={handleFileUpload}
-                                />
-                                {
-                                    productDetails.imageBase64URL.length > 0 && (
-                                        <div className="imagesPreview">
-                                            <div className="imagePreview">
-                                                <img src={productDetails.imageBase64URL} alt="" />
-                                                <span className="remove-image" onClick={removeUploadedImage}><GrClose size="8" /></span>
-                                            </div>
-                                        </div>
-                                    )
-                                }
-                            </div>
-                        </div>
-                        <div>
-                            <button
-                                type="submit"
-                                className="list-item-button"
-                            >
-                                List item
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </Modal>
+                    </Modal>
+                ) : (
+                    <Modal show={props.show} closeModal={props.closeModal} title="Sign in to list an item">
+                    </Modal>
+                )
+            }
+
         </React.Fragment>
     )
 }
