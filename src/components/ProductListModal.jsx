41ecf1aa2117e7db10679e091356d0e0f799654c
Pranay Namburi <pranayn1999@gmail.com>
2024-03-03 18:21:34 -0500

    Product listing working successfully

--- src/components/ProductListModal.jsx
+++ src/components/ProductListModal.jsx
@@ -4,40 +4,43 @@ import './../styles/ProductListModal.css'
 import { GrClose } from "react-icons/gr";
 import axios from "axios";
 import { useUser } from '../hooks/UserContext';
+import { useNavigate } from "react-router-dom";
 
 export const ProductListModal = (props) => {
-    const categories = [
-        {
-            id: 1,
-            name: "Electronics"
-        },
-        {
-            id: 2,
-            name: "Furniture"
-        },
-        {
-            id: 3,
-            name: "Vehicles"
-        },
-        {
-            id: 4,
-            name: "Fashion"
-        },
-        {
-            id: 5,
-            name: "Others"
-        }
-    ]
+    // const categories = [
+    //     {
+    //         id: 1,
+    //         name: "Electronics"
+    //     },
+    //     {
+    //         id: 2,
+    //         name: "Furniture"
+    //     },
+    //     {
+    //         id: 3,
+    //         name: "Vehicles"
+    //     },
+    //     {
+    //         id: 4,
+    //         name: "Fashion"
+    //     },
+    //     {
+    //         id: 5,
+    //         name: "Others"
+    //     }
+    // ]
+    const [categories, setCategories] = useState([])
     const [productDetails, setProductDetails] = useState({
         title: "",
         description: "",
-        price_per_day: "",
+        pricePerDay: "",
         categoryId: "",
         ownerId: "",
-        imageBase64URL: ""
+        imgBase64: ""
     })
     const imageInputRef = useRef(null);
     const { user, loginData } = useUser();
+    const navigate = useNavigate();
 
     const handleChange = (e) => {
         const { name, value } = e.target; // Destructure the name and value from the event target
@@ -58,7 +61,7 @@ export const ProductListModal = (props) => {
                 const base64String = reader.result;
                 setProductDetails({
                     ...productDetails,
-                    imageBase64URL: base64String
+                    imgBase64: base64String
                 });
             };
 
@@ -70,7 +73,7 @@ export const ProductListModal = (props) => {
         try {
             setProductDetails({
                 ...productDetails, // Spread the current formData
-                imageBase64URL: "" // Update the value of the specific field
+                imgBase64: "" // Update the value of the specific field
             });
             if (imageInputRef.current) {
                 imageInputRef.current.value = "";
@@ -83,21 +86,33 @@ export const ProductListModal = (props) => {
 
     const handleSubmitProductListDetails = useCallback(async (e) => {
         e.preventDefault()
-        console.log(productDetails)
+        console.log('product details to be submitted', productDetails)
+        const response = await axios.post(process.env.REACT_APP_PRODUCT_SERVICE + "/add-product", productDetails,
+            {
+                headers:
+                {
+                    "Content-Type": "application/json"
+                }
+            });
+        console.log(response)
+        if(response.status == 200) {
+            handleModalClose()
+        }
     })
 
     const handleModalClose = useCallback(() => {
         setProductDetails({
             title: "",
             description: "",
-            price_per_day: "",
+            pricePerDay: "",
             categoryId: "",
-            ownerId: user.user_id,
-            imageBase64URL: ""
+            ownerId: user?.user_id || "",
+            imgBase64: ""
         })
         props.closeModal()
     })
 
+    // Update owner id with logged in user's id
     useEffect(() => {
         console.log(user)
         if (user?.user_id) {
@@ -107,6 +122,27 @@ export const ProductListModal = (props) => {
             });
         }
     }, [user])
+
+    // Get all available categories 
+    useEffect(() => {
+        const getAvailableCategories = async () => {
+            try {
+                const response = await axios.get(process.env.REACT_APP_PRODUCT_SERVICE + "/categories");
+                console.log("getAvailableCategories response", response?.data?.data)
+                const availableCategories = response.data.data
+                setCategories(availableCategories)
+
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getAvailableCategories();
+    }, [])
+
+    const goToLoginPage = useCallback(() => {
+        navigate('/login')
+        handleModalClose()
+    })
 
     return (
         <React.Fragment>
@@ -154,9 +190,9 @@ export const ProductListModal = (props) => {
                                     <div className="input-wrapper">
                                         <input
                                             id="price"
-                                            name="price_per_day"
+                                            name="pricePerDay"
                                             type="number"
-                                            value={productDetails.price_per_day}
+                                            value={productDetails.pricePerDay}
                                             placeholder="Enter your product price per day"
                                             required
                                             onChange={handleChange}
@@ -191,10 +227,10 @@ export const ProductListModal = (props) => {
                                             onChange={handleFileUpload}
                                         />
                                         {
-                                            productDetails.imageBase64URL.length > 0 && (
+                                            productDetails.imgBase64.length > 0 && (
                                                 <div className="imagesPreview">
                                                     <div className="imagePreview">
-                                                        <img src={productDetails.imageBase64URL} alt="" />
+                                                        <img src={productDetails.imgBase64} alt="" />
                                                         <span className="remove-image" onClick={removeUploadedImage}><GrClose size="8" /></span>
                                                     </div>
                                                 </div>
@@ -214,7 +250,12 @@ export const ProductListModal = (props) => {
                         </div>
                     </Modal>
                 ) : (
-                    <Modal show={props.show} closeModal={props.closeModal} title="Sign in to list an item">
+                    <Modal show={props.show} closeModal={handleModalClose} title="Sign in to list an item">
+                        <div className="sign-in-section">
+                            <button onClick={goToLoginPage}>
+                                Sign In
+                            </button>
+                        </div>
                     </Modal>
                 )
             }
