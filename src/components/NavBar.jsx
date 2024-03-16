e8e39abdb15eeb84a1c754b84314326b3e1b89dc
Pranay Namburi <pranayn1999@gmail.com>
2024-03-16 01:07:54 -0400

    NavBar user profile dropdown

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,31 @@
-import { useCallback } from 'react';
+import { useCallback, useState, useEffect } from 'react';
 import { Navigate, useNavigate } from "react-router-dom";
 import './../styles/NavBar.css'
 import { useUser } from '../hooks/UserContext';
+import { CgProfile } from "react-icons/cg";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 
 export const NavBar = (props) => {
     const navigate = useNavigate();
-    const { loginData, logOutUser } = useUser();
+    const { loginData, logOutUser, user } = useUser();
+    const [showProfileDropDown, setShowProfileDropDown] = useState(false)
+    const toggleProfileDropdown = useCallback(() => {
+        setShowProfileDropDown(!showProfileDropDown)
+    })
     const goToLoginPage = useCallback(() => {
         navigate('/login')
     })
     const goToHomePage = useCallback(() => {
         navigate('/')
     })
-    const handleLogout = useCallback(async() => {
+    const handleLogout = useCallback(async () => {
         await logOutUser()
         goToHomePage()
     })
 
     return (
         <div className="NavBar">
-            <div onClick = {goToHomePage}>
+            <div onClick={goToHomePage}>
                 <img
                     className="navbar-company-logo content-brandLogo"
                     alt="RentItAll"
@@ -30,12 +36,31 @@ export const NavBar = (props) => {
                     List your item
                 </button>
                 {
-                    loginData.isLoggedIn ? (
-                        <button
-                            onClick={handleLogout}
-                        >
-                            Sign Out
-                        </button>
+                    loginData.isLoggedIn && user ? (
+                        <div className='user-profile-wrapper' onClick={toggleProfileDropdown}>
+                            <div className='user-profile'>
+                                <div className='user-profile-name' title={user.firstName + ' ' + user.lastName}><CgProfile color='white' size='32' /> {user.firstName + ' ' + user.lastName}</div>
+                                {
+                                    showProfileDropDown ? <IoIosArrowUp color='white' /> :
+                                        <IoIosArrowDown color='white' />
+                                }
+                            </div>
+                            {showProfileDropDown &&
+                                <div className='user-profile-dropdown'>
+                                    <div className='each-action'>Your Account</div>
+                                    {/* <div className='each-action'></div>
+                                    <div className='each-action'></div> */}
+                                    <div className='each-action'>
+                                        <button
+                                            onClick={handleLogout}
+                                            className='sign-out-button'
+                                        >
+                                            Sign Out
+                                        </button>
+                                    </div>
+                                </div>
+                            }
+                        </div>
                     ) : (
                         <button
                             onClick={goToLoginPage}
